945a7ed152fb076b09ec8ecc8d6edaa2cd0acd54
Viktor Jackson <viktor.jackson@gmail.com>
2012-08-09 09:33:27 +0200

    Move templates to views, split app.php to init.php

--- src/app.php
+++ src/app.php
@@ -1,35 +1,41 @@
 <?php
 
-use Lennux\LoginServiceProvider;
-use Silex\SessionServiceProvider;
-use Silex\Provider\TwigServiceProvider;
-use Silex\Provider\DoctrineServiceProvider;
-use Silex\Application;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+require_once __DIR__.'/init.php';
 
-/** Bootstrap **/
-require_once __DIR__.'/../vendor/autoload.php';
-$path = pathinfo($_SERVER['SCRIPT_NAME']);
-$app = new Application();
-$app['debug'] = true;
-$app['base'] = ($path['dirname'] != "/" ? $path['dirname'] . "/" : "/" );
-$app->register(new TwigServiceProvider(), array('twig.path' => __DIR__.'/templates'));
-$app->register(new DoctrineServiceProvider(), array(
-    'db.options' => array(
-        'driver' => 'pdo_sqlite',
-        'path'   => __DIR__.'/glry.db',
-    ),  
-));
+/** Routes */
+    $app->get('/', function() use($app){
+        return $app['twig']->render('collections.html', array(
+            'collections' => $app['galleryService']->getCollections()
+        ));
+    });
 
-/** App Definition */
-$app->get('/', function() use($app){
-    return $app['twig']->render('collections.html');
-});
+    $app->get('/c/{collection_id}', function($collection_id) use ($app){
+        return $app['twig']->render('images.html', array(
+            'collection' => $app['galleryService']->getCollection($collection_id),
+            'collection_id' => $collection_id
+        ));
+    });
 
-$app->get('/c/{collection}', function($collection) use ($app){
-    return $app['twig']->render('images.html', array('collection' => $collection));
-});
+    $app->post('/install', function(Request $request) use ($app) {
+        $app['login']->storeHash($request->get('pass'));
+        /* return $app->redirect($app['base']); */
+    }); 
 
+    $app->get('/install', function() use ($app) {
+        if($app['login']->checkAuth())
+            throw new Exception('Password is already set');
+        return $app['twig']->render('install.html');
+    });
+
+    $app->post('/login', function (Request $request) use($app) {
+        if($app['login']->checkPass($request->get('pass')))
+            $app['session']->set("login", 1);
+
+        return $app->redirect($app['base']);
+    });
+    
+    $app->get('/new/{token}', function(Request $request) use($app) {
+        var_dump($request);
+        var_dump($app['session']);
+    });
 return $app;
-
